ed5ff667c6aef0d5538290a7546af3b7c849a7bd
joseph-emmanuel <joseph.emmanuel@outlook.com>
2020-07-29 05:15:34 -0400

    added features for some html and css

--- Activity_1_Joseph_Emmanuel/js/Tic-Tac-Toe.js
+++ Activity_1_Joseph_Emmanuel/js/Tic-Tac-Toe.js
@@ -1,27 +1,71 @@
 let firstUserSelcted;
 let secondUserSelected;
-let totalClicks;
+let totalClicks = 0;
+let clickedButton;
+let xArray = [];
+let oArray = [];
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+document.getElementById("gameBoard").style.visibility = "hidden";
+document.getElementById("cross").onclick = function () {
+  addUserChoice(1);
+};
+document.getElementById("circle").onclick = function () {
+  addUserChoice(2);
+};
 
+const wrapper = document.getElementById("gameBoard");
 
-
-
-
-document.getElementById("cross").onclick = function() {addUserChoice()};
-document.getElementById("circle").onclick = function() {addUserChoice()};
-
-function addUserChoice() {
-    if(document.getElementById("cross").innerText=='X'){
-        firstUserSelcted='X'
-        secondUserSelected='O'
-    }else if(document.getElementById("cross").innerText=='0'){
-        firstUserSelcted='O'
-        secondUserSelected='X'
-    }
-  document.getElementById("cross").style.visibility='hidden'
-  document.getElementById("circle").style.visibility='hidden' 
+wrapper.addEventListener("click", (event) => {
+  const isButton = event.target.nodeName === "BUTTON";
+  if (!isButton) {
+    return;
+  }
+  clicksDone(event.target);
+});
+function addUserChoice(choice) {
+  if (choice == 1) {
+    firstUserSelcted = "X";
+    secondUserSelected = "O";
+  } else if (choice == 2) {
+    firstUserSelcted = "O";
+    secondUserSelected = "X";
+  }
+  document.getElementById("cross").style.visibility = "hidden";
+  document.getElementById("circle").style.visibility = "hidden";
+  document.getElementById("mainHead").style.visibility = "hidden";
+  document.getElementById("gameBoard").style.visibility = "visible";
 }
-function clicksDone(){
-    let symbol=''
-    totalClicks=totalClicks+1
-
+function clicksDone(card) {
+  //   console.log(card);
+  let symbol = "";
+  totalClicks = totalClicks + 1;
+  //   console.log(totalClicks);
+  card.innerText = totalClicks % 2 == 0 ? secondUserSelected : firstUserSelcted;
+  if (card.innerText == "X") {
+    xArray.push(parseInt(card.id));
+  } else {
+    oArray.push(parseInt(card.id));
+  }
+  //   card.style.backgroundColor = "gray";
+  card.classList.add("disabled");
+  for (let index = 0; index < winningConditions.length; index++) {
+    if (JSON.stringify(winningConditions[index]) == JSON.stringify(xArray)) {
+      document.getElementById("mainHead").innerHTML = "User 1 won the game";
+      document.getElementById("mainHead").style.visibility = "visible";
+    } else if (
+      JSON.stringify(winningConditions[index]) == JSON.stringify(oArray)
+    ) {
+      document.getElementById("mainHead").innerHTML = "User 2 won the game";
+      document.getElementById("mainHead").style.visibility = "visible";
+    }
+  }
 }
